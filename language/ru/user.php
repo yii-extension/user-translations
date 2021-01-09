c51d213760beb70b3a895aebff67a88555506120
Иван <68641750+Mister-42@users.noreply.github.com>
2021-01-10 00:57:43 +0100

    Adding translations (#2)

--- language/ru/user.php
+++ language/ru/user.php
@@ -2,37 +2,42 @@
 
 declare(strict_types=1);
 
-// This file has been created using Google Translate.
-// Feel free to correct the translations and remove this message once proper
-// translations have been provided.
+// This file has been created with the help of Google Translate and probaly contains errors.
+// Feel free to correct this file and remove this message once proper translations have been provided.
 
 return [
     // Translations flash message
-    'Please check your email to activate your username' => '',
-    'Please check your email to change your password' => '',
-    'Sign in successful' => '',
-    'Sign in successful - you are welcome' => '',
-    'System Notification - Yii Demo User Module AR' => '',
-    'Your account has been created' => '',
-    'Your data has been saved' => '',
-    'Your password has been changed' => '',
+    'Please check your email to activate your username' => 'Пожалуйста, проверьте свою электронную почту, чтобы активировать свое имя пользователя',
+    'Please check your email to change your password' => 'Пожалуйста, проверьте свою электронную почту, чтобы изменить пароль',
+    'Sign in successful' => 'Успешный вход',
+    'Sign in successful - you are welcome' => 'Успешный вход - Добро пожаловать',
+    'System Notification' => 'Системное уведомление',
+    'Your account has been created' => 'Ваш аккаунт был создан',
+    'Your data has been saved' => 'Ваши данные были сохранены',
+    'Your password has been changed' => 'Ваш пароль был изменен',
 
     // Translations views
     'Already registered - Sign in!' => 'Уже зарегистрированы - Vходить!',
+    'Bio' => 'Биография',
     'Continue' => 'Продолжить',
     "Don't have an account - Sign up!" => "Нет учетной записи - Регистрация!",
     "Didn't receive confirmation message" => "Не получил подтверждающее сообщение",
     'Email' => 'Эл. адрес',
     'Forgot password' => 'Забыли пароль',
     'Go Back Home' => 'Вернуться Главная',
+    'Location' => 'Расположение',
     'Login' => 'Вход',
+    'Name' => 'Имя',
     'Password' => 'Пароль',
     'Please contact us if you t1hink this is a server error. Thank you.' => 'Свяжитесь с нами, если вы считаете, что это ошибка сервера. Спасибо.',
+    'Public email' => 'Общедоступный эл. адрес',
     'Remember me' => 'Запомнить',
     'Register' => 'Регистрация',
     'Resend confirmation message' => 'Отправить сообщение с подтверждением еще раз',
     'Reset password' => 'Сброс пароля',
     'The above error occurred while the Web server was processing your request.' => 'Вышеупомянутая ошибка произошла во время обработки вашего запроса сервером.',
     'The page %s was not found.' => 'Страница %s не найдена.',
+    'Time zone' => 'Часовой пояс',
     'Username' => 'Имя пользователя',
+    'Website' => 'Веб-сайт',
 ];
