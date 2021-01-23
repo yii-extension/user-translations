1dca1dd495f27d6e402c566dbba258e532b1dc28
Mr.42 <johan@mr42.me>
2021-01-24 00:39:34 +0100

    Extract user-mailer

--- extract.sh
+++ extract.sh
@@ -7,10 +7,16 @@ find vendor/yii-extension/user/src/ -name *.php | xargs xgettext --from-code=utf
 find vendor/yii-extension/user-view-bulma/storage/ -name *.php | xargs xgettext --from-code=utf-8 --language=PHP --no-location --omit-header --sort-output --keyword=translate --output="locales/user-view.pot"
 find vendor/yii-extension/user-view-bootstrap5/storage/ -name *.php | xargs xgettext --from-code=utf-8 --language=PHP --no-location --omit-header --sort-output --keyword=translate --output="locales/user-view.pot"
 
+# user-mailer
+find vendor/yii-extension/user-mailer-service/storage/ -name *.php | xargs xgettext --from-code=utf-8 --language=PHP --no-location --omit-header --sort-output --keyword=translate --output="locales/user-mailer.pot"
+
 # merge
 for i in locales/*/user.po; do
 	msgmerge --update --silent "$i" locales/user.pot
 done
 for i in locales/*/user-view.po; do
 	msgmerge --update --silent "$i" locales/user-view.pot
+done
+for i in locales/*/user-mailer.po; do
+	msgmerge --update --silent "$i" locales/user-mailer.pot
 done
